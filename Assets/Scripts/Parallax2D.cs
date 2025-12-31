afa9ed353226e58eb00704344878454df7fe9f5b
plantcraft94 <plantcraft60@gmail.com>
2025-12-31 13:47:40 +0700

    new background

--- Assets/Scripts/Parallax2D.cs
+++ Assets/Scripts/Parallax2D.cs
@@ -3,33 +3,37 @@ using UnityEngine;
 public class Parallax2D : MonoBehaviour
 {
 
-	float StartPos, Length;
-	GameObject cam;
-	[SerializeField] float ParallaxEffectSpeed;
-	public float distance;
+	float startX;
+    float length;
 
-	float movement;
-	private void Start()
-	{
+    Transform cam;
 
-		StartPos = transform.position.x;
-		cam = Camera.main.gameObject;
-		Length = GetComponent<SpriteRenderer>().bounds.size.x;
-	}
-	private void FixedUpdate()
-	{
-		distance = cam.transform.position.x * ParallaxEffectSpeed;
-		movement = cam.transform.position.x * (1 - ParallaxEffectSpeed);
+    [Range(0f, 1f)]
+    [SerializeField] float parallaxEffectSpeed = 0.5f;
 
-		transform.position = new Vector3(StartPos + distance, transform.position.y, transform.position.z);
+    void Start()
+    {
+        cam = Camera.main.transform;
+        startX = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
 
-		if (movement > StartPos + Length)
-		{
-			StartPos += Length;
-		}
-		else if (movement <= StartPos - Length)
-		{
-			StartPos -= Length;
-		}
-	}
+    void LateUpdate()
+    {
+        // Parallax movement
+        float parallaxX = cam.position.x * parallaxEffectSpeed;
+        transform.position = new Vector3(startX + parallaxX, transform.position.y, transform.position.z);
+
+        // Infinite scrolling
+        float temp = cam.position.x * (1 - parallaxEffectSpeed);
+
+        if (temp > startX + length)
+        {
+            startX += length;
+        }
+        else if (temp < startX - length)
+        {
+            startX -= length;
+        }
+    }
 }
